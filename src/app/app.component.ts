fe9e7d672c44de18297bd6c6441373b80224134b
Daniel Leon <danielmatus468@gmail.com>
2023-08-01 23:12:59 -0600

    Se agrego boostrap al proyecto y se creo el header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Curso FullStack Angular & Spring Boot';
+  curso = 'Angular & Spring Boot: Creando web app full stack';
+  author = 'Andres Guzman';
 }
